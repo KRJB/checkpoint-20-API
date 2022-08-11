81aa7226b589ee96dfb245d24fe90cf892c4f768
KRJB <khalilrejeb0@gmail.com>
2022-08-11 09:44:24 +0100

    first commi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel'
+import './App.css'
 function App() {
+  const [user, setUser] = useState([])
+  useEffect(() =>{
+      axios.get('https://jsonplaceholder.typicode.com/users')
+
+          .then(result => {
+              setUser(result.data)
+          })
+          .catch(error => console.log('error is ', error))}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+<Carousel>
+
+{
+    user.map(el =>
+        <Carousel.Item key={el.id}>
+            <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1553095066-5014bc7b7f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbCUyMGJhY2tncm91bmR8ZW58MHx8MHx8&w=1000&q=80"
+                alt={el.name}
+            />
+            <Carousel.Caption className="caroussel_caption__styling">
+                <div className="list__styling">
+                    <p><span className="prefix_styling">Name : </span>{el.name}</p>
+                    <p><span className="prefix_styling">Username : </span>{el.username}</p>
+                    <p><span className="prefix_styling">Email : </span>{el.email}</p>
+                    <p><span className="prefix_styling">Street : </span>{el.address.street}</p>
+                    <p><span className="prefix_styling">Suite : </span>{el.address.suite}</p>
+                    <p><span className="prefix_styling">City : </span>{el.address.city}</p>
+                </div>
+
+            </Carousel.Caption>
+
+         </Carousel.Item>
+        )
+}
+
+</Carousel>
+   </div>
   );
 }
 
